e52aad35022924648150a8bce56391ba74bab40b
Tanbui <hoangtan223@gmail.com>
2016-07-25 03:39:25 +0700

    Finish implement purchase tick function - without test

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -7,10 +7,29 @@ class TicketsController < ApplicationController
     @event = Event.find(params[:event_id])
     if @event.is_out_of_date?
       flash[:error] = "Booking closed! The event is started"
-      redirect_to new_event_ticket_path
+      render 'new'
+      return
     end
     
-    ticket_quantity = params[:ticket_quantity]
+    ticket_quantities = params[:ticket_quantity]
+    @order = Order.new(event: @event)
 
+    ticket_quantities.each do |ticket_type_id, quantity|
+      ticket_type = TicketType.find(ticket_type_id)
+      order_detail = OrderDetail.new(ticket_type_id: ticket_type_id, quantity: quantity)
+      unless ticket_type.enough_quantity? order_detail.quantity
+        flash[:error] = "There's only #{ticket_type.remaining_tickets} tickets left for #{ticket_type.name}"
+        render 'new'
+        return
+      end
+      @order.order_details << order_detail
+    end
+
+    if @order.save
+      redirect_to root_path
+    else
+      flash[:error] = @order.errors.full_messages.to_sentence
+      render 'new'
+    end
   end
 end
--- app/models/ticket_type.rb
+++ app/models/ticket_type.rb
@@ -1,5 +1,6 @@
 class TicketType < ActiveRecord::Base
   belongs_to :event
+  has_many :order_details
 
   before_create :check_duplicates
 
@@ -7,5 +8,14 @@ class TicketType < ActiveRecord::Base
     if TicketType.exists?(event: event, price: price)
       errors.add(:base, "cannot have duplicates")
     end
+  end
+
+  def remaining_tickets
+    sold_quantity = order_details.sum(:quantity)
+    max_quantity - sold_quantity
+  end
+
+  def enough_quantity?(quantity)
+     remaining_tickets - quantity < 0 ? false : true
   end
 end
