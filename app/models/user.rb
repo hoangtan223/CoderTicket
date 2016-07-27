ec07e24c020d3426ef7a7ddead61be775f2b078a
Tanbui <hoangtan223@gmail.com>
2016-07-27 12:54:51 +0700

    Add creator_id to Event table and add associons

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :created_events, foreign_key: 'creator_id', class_name: 'Event'
 
   validates :name, presence: true
   validates :email, uniqueness: true
