120ce549fdb728bfc4e74d0a1797949f986301a9
Tanbui <hoangtan223@gmail.com>
2016-07-31 00:13:45 +0700

    Add link to event details

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,12 @@ module EventsHelper
   def header_background_image_url
     "https://az810058.vo.msecnd.net/site/global/Content/img/home-search-bg-0#{rand(6)}.jpg"
   end
+
+  def is_creator_of?(event)
+    if is_logged_in?
+      current_user.id == event.creator_id
+    else
+      return false
+    end
+  end
 end
